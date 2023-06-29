36c430cc5da4c4d7efe60dfe51ad7ce90ed4934f
Kroopla <70652725+Kroopla@users.noreply.github.com>
2023-06-29 14:11:09 +0200

    dodawanie częsciowo zrobione

--- main.py
+++ main.py
@@ -5,16 +5,23 @@ ws = wb.active
 petla = True
 def dodaj():
     print("\nProgramistyczna dupa\n")
+    kwota = input("Podaj kwote: ")
+    data = input("Data: ")
+    ws["A" + str(3+int(ws["D1"].value))] = int(kwota)
+    ws["B" + str(3+int(ws["D1"].value))] = str(data)
+    ws["D1"] = int(ws["D1"].value) + 1
+    ws["B1"] = int(ws["B1"].value) + int(kwota)
+    wb.save("test.xlsx")
 
 def wyswietl_wszystko():
     for x in range(3, 3+int(ws["D1"].value)):
-        print(str(ws["A" + str(x)].value))
+        print(str(ws["A" + str(x)].value) + ", " + str(ws["B" + str(x)].value))
 
 
 
 if __name__ == '__main__':
     while petla:
-        print("Saldo:" + str(ws["A2"].value))
+        print("\nSaldo:" + str(ws["B1"].value))
         n = int(input("Co chcesz zrobić?\n"
                     "1. Dodaj wydatek lub przychód\n"
                     "2. Wyświetl wszystkie wydatki\n"
