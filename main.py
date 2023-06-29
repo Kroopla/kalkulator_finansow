7b702dfe6f35b80b324c0aed2f693c6aba071719
Kroopla <70652725+Kroopla@users.noreply.github.com>
2023-06-29 18:59:32 +0200

    polepszenie warstwy wizualnej

--- main.py
+++ main.py
@@ -19,9 +19,10 @@ def dodaj():
     wb.save("test.xlsx")
 
 def wyswietl_wszystko():
-    for x in range(2, 3+int(ws["D1"].value)):
-        print(str(ws["A" + str(x)].value) + ", " + str(ws["B" + str(x)].value) + ", " +
-              str(ws["C" + str(x)].value) + ", " + str(ws["D" + str(x)].value))
+    print(str(ws["A2"].value) + "\t" + str(ws["B2"].value) + "\t\t" + str(ws["C2"].value) + "\t" + str(ws["D2"].value))
+    for x in range(3, 3+int(ws["D1"].value)):
+        print(str(ws["A" + str(x)].value) + ",\t" + str(ws["B" + str(x)].value) + ",\t" +
+              str(ws["C" + str(x)].value) + ",\t\t" + str(ws["D" + str(x)].value))
 
 def wyczysc_wszystko():
     for x in range(3, 3+int(ws["D1"].value)):
@@ -34,16 +35,22 @@ if __name__ == '__main__':
     while petla:
         print("\nSaldo:" + str(ws["B1"].value))
         n = int(input("Co chcesz zrobić?\n"
-                    "1. Dodaj wydatek lub przychód\n"
+                    "1. Dodaj wydatek\n"
                     "2. Wyświetl wszystkie wydatki\n"
-                    "3. Wyczyść cały kalkulator\n"
+                    "9. Wyczyść cały kalkulator\n"
                     "0. Zakończ program\n"))
         if n == 1:
             dodaj()
         elif n == 2:
             wyswietl_wszystko()
-        elif n == 3:
-            wyczysc_wszystko()
+        elif n == 9:
+            print("Na pewno chcesz wyczyścić całą historię? Sracisz w ten sposób wszyskie dane")
+            w = int(input("1. Nie! Nie chcę stracić moich cennych danych\n"
+                          "9. Tak, chcę wyczyścić całą historię i zacząć od zera\n"))
+            if w == 9:
+                wyczysc_wszystko()
+            elif n == 1:
+                continue
         elif n == 0:
             print("PaPa...\n")
             exit()
