e949f5e7cb93bf5b37d657a85fb314f268d72790
Kroopla <70652725+Kroopla@users.noreply.github.com>
2023-06-30 15:09:32 +0200

    wykresik

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from openpyxl.reader.excel import load_workbook
 import datetime
+import numpy as np
+import matplotlib.pyplot as plt
 
 wb = load_workbook("test.xlsx")
 ws = wb.active
@@ -29,8 +31,9 @@ def wyswietl(x):
 def wyczysc_wszystko():
     for x in range(3, 3 + int(ws["D1"].value)):
         ws.delete_rows(3)
-        ws["D1"] = 0
-        ws["B1"] = 0
+
+    ws["D1"] = 0
+    ws["B1"] = 0
     wb.save("test.xlsx")
 
 
@@ -48,6 +51,7 @@ def szukaj_data():
                 ws["B" + str(x)].value)[6:10]:
             wyswietl(x)
 
+
 def okres():
     dzien_p = int(input("Dzień początkowy: "))
     miesiac_p = int(input("Miesiąc początkowy: "))
@@ -58,12 +62,14 @@ def okres():
     miesiac_k = int(input("Miesiąc końcowy: "))
     rok_k = int(input("Rok końcowy: "))
     data_k = datetime.date(rok_k, miesiac_k, dzien_k)
+
     print(str(ws["A2"].value) + "\t" + str(ws["B2"].value) + "\t\t" + str(ws["C2"].value) + "\t" + str(ws["D2"].value))
     for x in range(3, 3 + int(ws["D1"].value)):
         data_x = datetime.date(int(str(ws["B" + str(x)].value)[6:10]), int(str(ws["B" + str(x)].value)[3:5]),
                                int(str(ws["B" + str(x)].value)[0:2]))
         if data_p <= data_x <= data_k:
             wyswietl(x)
+
 
 def szukaj_kategoria():
     kategoria = input("Karegoria: ")
@@ -73,12 +79,23 @@ def szukaj_kategoria():
             wyswietl(x)
 
 
+def wykres():
+    x = []
+    y = []
+    for k in range(3, 3 + int(ws["D1"].value)):
+        x.append(str(ws["B" + str(k)].value))
+        y.append(str(ws["A" + str(k)].value))
+    plt.plot(x, y)
+    plt.show()
+
+
 if __name__ == '__main__':
     while True:
         n = input("\nCo chcesz zrobić?\n"
                   "1. Dodaj wydatek\n"
                   "2. Wyświetl wszystkie wydatki\n"
                   "3. Szukaj\n"
+                  "4. Wykres \n"
                   "9. Wyczyść cały kalkulator\n"
                   "0. Zakończ program\n")
         if n == "1":
@@ -103,6 +120,8 @@ if __name__ == '__main__':
             else:
                 continue
 
+        elif n == "4":
+            wykres()
         elif n == "9":
             print("Na pewno chcesz wyczyścić całą historię? Sracisz w ten sposób wszyskie dane")
             w = input("(Dowolna wartość) Nie! Nie chcę stracić moich cennych danych\n"
